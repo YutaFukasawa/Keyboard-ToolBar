1e55a3fff8f1a134995d6a898b5b938d3284f816
Yuta <yfukasawa@sonix.asia>
2018-12-18 15:32:44 +0900

    create DekoeViewController.swift

---
+++ KeyboardToolBar/KeyboardToolBar/DekoeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DekoeViewController.swift
+//  KeyboardToolBar
+//
+//  Created by yfukasawa on 2018/12/18.
+//  Copyright © 2018 yfukasawa. All rights reserved.
+//
+
+import UIKit
+
+class DekoeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
