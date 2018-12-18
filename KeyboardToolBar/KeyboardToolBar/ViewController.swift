95480989c0077fc294560bb55aa48b6702aafdda
Yuta <yfukasawa@sonix.asia>
2018-12-18 12:36:32 +0900

    add TextView

--- KeyboardToolBar/KeyboardToolBar/ViewController.swift
+++ KeyboardToolBar/KeyboardToolBar/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    let textView = UITextView()
+    textView.frame = CGRect(x: 0, y: 150, width: 500, height: 500)
+    textView.backgroundColor = UIColor.brown
+    view.addSubview(textView)
   }
+
+//  func keyboardToolBar() {
+//    let toolBar = UIToolbar()
+//    let inputButton = UIBarButtonItem(title: "入力", style: .plain, target: self, action: #selector(inputTapped))
+//    let dekoeButton = UIBarButtonItem(title: "デコ絵", style: .plain, target: self, action: #selector(inputTapped))
+//    let dekogaButton = UIBarButtonItem(title: "デコ画", style: .plain, target: self, action: #selector(inputTapped))
+//    let imageButton = UIBarButtonItem(title: "写真", style: .plain, target: self, action: #selector(inputTapped))
+//    let sousyokuButton = UIBarButtonItem(title: "装飾", style: .plain, target: self, action: #selector(inputTapped))
+//    let okButton = UIBarButtonItem(title: "OK", style: .plain, target: self, action: #selector(inputTapped))
+//  }
+//
+//  func inputTapped() {
+//
+//  }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
